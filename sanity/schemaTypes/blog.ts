df9d36a334883e4de4b621daebf42794c1d636dd
samsher2578 <samsherkhan2578@gmail.com>
2024-02-15 10:24:55 +0530

    add schema

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,42 @@
+import {title} from 'process'
+
+export default {
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title of blog artical',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug of your blog artical',
+      options: {
+        source: 'title',
+      },
+    },
+    {
+      name: 'titleImage',
+      type: 'image',
+      title: 'Title Image',
+    },
+    {
+      name: 'smallDescription',
+      type: 'text',
+      title: 'Small Description',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    },
+  ],
+}
